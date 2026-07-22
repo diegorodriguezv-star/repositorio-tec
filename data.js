570bba6c3c0217c0455ecd91845ff9dde508bf7f
diegorodriguezv-star <diego.rodriguezv@colsubsidio.edu.co>
2026-07-21 21:42:05 -0500

    Update SEED_VERSION and change image path

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 // Datos de ejemplo del repositorio (simulan lo que vendría de una base de datos real)
 // Cada recurso sigue los campos de Dublin Core que se documentan en el trabajo escrito.
-const SEED_VERSION = 5;
+const SEED_VERSION = 6;
 const COLLECTIONS = [
   { id: "electronica", name: "Electrónica básica", icon: "chip" },
   { id: "tinkercad", name: "Tinkercad circuitos", icon: "circuit" },
@@ -23,7 +23,7 @@ const SEED_RESOURCES = [
     link: "https://www.tinkercad.com/things/cVJfRWcTtLe-un-pulsador-enciende-y-apaga-un-led/editel?returnTo=https%3A%2F%2Fwww.tinkercad.com%2Fdashboard%2Fdesigns%2Fcircuits&sharecode=Alpq5uW7GTiKmOZ46V2mwJb1Pp-aOzx9Y6trdFO32jM",
     // Ruta relativa a la carpeta "images/" dentro del repositorio.
     // Deja "" si todavía no tienes la imagen para este recurso.
-    image: "images/circuito-led.png",
+    image: "images/tinkercad.png",
     status: "publicado",
   },
   {
